0e2a71ab9d536a1efa51494dd98ef7ad368c5895
liangxiaojun <fatalxiao@163.com>
2021-07-26 14:43:56 +0800

    update example

--- examples/withAsyncComponentPlugin/src/index.js
+++ examples/withAsyncComponentPlugin/src/index.js
@@ -12,17 +12,24 @@ import {configureRoutes} from './routes';
 
 // Import Vivy
 import Vivy from 'vivy';
+import VivyRouter from 'vivy-router';
 import VivyAsyncComponent from '../../../src';
 
 // Create browser history
 const history = createBrowserHistory();
 
 // Create vivy
-const vivy = Vivy(history);
+const vivy = Vivy();
+
+// Apply router plugin
+vivy.use(VivyRouter({
+    history
+}));
 
 // Apply async component plugin
 vivy.use(VivyAsyncComponent({
     // Customized AsyncComponentLoading model nameSpace
+    // default is "asyncComponentLoading"
     modelNameSpace: 'customizedAsyncComponentLoading'
 }));
 
