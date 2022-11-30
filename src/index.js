b5e520db2620daa5406767c1c36d3790e0871ad9
liangxiaojun <fatalxiao@163.com>
2022-11-30 16:13:50 +0800

    optimize babel config

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import createAsyncComponentLoading from './models/asyncComponentLoading';
 import createAsyncComponentLoadingMiddleware from './middlewares/AsyncComponentLoadingMiddleware';
 
 // Components
-export AsyncComponent from './components/AsyncComponent';
+export * from './components/AsyncComponent';
 
 /**
  * Default Vivy async component options
